336f57ee40b5560e9d764b8590415c533568b057
Peter Szabo <asdwsda@gmail.com>
2020-04-02 17:31:39 +0200

    Generated docs update script

---
+++ scripts/update-generated-docs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euf
+
+ROOT="$(git rev-parse --show-toplevel)"
+
+RELEASE_TAG="$1"
+BRANCH="update-generated-docs-${RELEASE_TAG}"
+
+function update_docs()
+{
+  mkdir -p tmp
+  git clone --depth 1 -b "${RELEASE_TAG}" "https://github.com/banzaicloud/logging-operator.git" "tmp/logging-operator"
+  cd 'tmp/logging-operator/'
+  rm -rf ./docs/{crds,plugins}
+  make docs
+  cp -R ./docs/{crds,plugins} "${ROOT}/docs/"
+  cd -
+  rm -rf tmp
+}
+
+function main()
+{
+  git checkout -b "${BRANCH}"
+  update_docs
+  git add --all
+  if git commit --dry-run; then
+    git commit -m "Update generated docs (${RELEASE_TAG})"
+    git checkout master
+    git merge "${BRANCH}"
+    git push origin master
+  else
+    echo "Nothing has changed."
+    circleci-agent step halt
+    exit 0
+  fi
+}
+
+main
